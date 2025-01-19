a07c857881dc6fc6baec989c642a226be2bd231a
decodeitorg <decodeit.orgl@gmail.com>
2025-01-19 23:28:08 +0600

    port updated

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+
+const { PORT } = loadEnv(process.env.NODE_ENV, '../../', '');
+console.log('🚀 ~ PORT:', PORT);
 
 export default defineConfig({
   integrations: [
@@ -18,7 +22,7 @@ export default defineConfig({
     },
     envDir: '../../',
   },
-  server: { port: 3210 },
+  server: { port: Number(PORT) },
   adapter: node({
     mode: 'standalone',
   }),
